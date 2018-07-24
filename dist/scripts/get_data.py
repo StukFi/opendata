3c55c5d6cb0e3da636ec467085d67db719d39ed7
Lasse Malmberg <ext-lasse.malmberg@stuk.fi>
2018-07-24 14:46:28 +0300

    Change timespan argument's format
    The argument's short version is now a single character long as that
    seems to be how it's usually done.

--- dist/scripts/get_data.py
+++ dist/scripts/get_data.py
@@ -15,8 +15,8 @@ def get_program_arguments():
     parser = argparse.ArgumentParser(description="Gets data from FMI's open data API.")
     parser.add_argument("data_type", choices=["dose_rates", "samplers"],
                         help="type of data to get")
-    parser.add_argument("-ts", "--timespan", nargs=2, metavar=('FROM', 'TO'),
-                        help="timespan for which to get data [YYYY-MM-DDThh:mm:ss]")
+    parser.add_argument("-s", "--timespan", nargs=2, metavar=('FROM', 'TO'),
+                        help="define timespan for which to get data [YYYY-MM-DDThh:mm:ss]")
     parser.add_argument("-a", "--auth", action="store_true",
                         help="use an API key to authenticate requests")
     return parser.parse_args()
