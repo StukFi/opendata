86e0b560944e045b58ccc6a99d678c0dd4a5410f
Lasse Malmberg <ext-lasse.malmberg@stuk.fi>
2019-03-06 13:57:16 +0200

    Add unit tests for Timepicker component

---
+++ src/components/__tests__/TimepickerWidget.spec.js
@@ -0,0 +1,54 @@
+import { shallowMount, createLocalVue } from "@vue/test-utils"
+import Vuex from "vuex"
+import TimepickerWidget from "components/TimepickerWidget.vue"
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe("TimepickerWidget.vue", () => {
+    let store
+    let storeOptions
+    let wrapper
+
+    beforeEach(() => {
+        storeOptions = {
+            modules: {
+                datetime: {
+                    state: {
+                        time: "1200",
+                        validDatetimes: []
+                    },
+                    getters: {
+                        validTimesForCurrentDate: jest.fn(() => { return []})
+                    }
+                },
+                settings: {
+                    state: {
+                        timeFormat: "fi"
+                    }
+                }
+            }
+        }
+
+        store = new Vuex.Store(storeOptions)
+        wrapper = shallowMount(TimepickerWidget, { localVue, store })
+    })
+
+    it("dispatches incrementTime when the increment button is clicked", () => {
+        const button = wrapper.find(".button__increment-time")
+        store.dispatch = jest.fn()
+
+        button.trigger("click")
+
+        expect(store.dispatch).toHaveBeenCalledWith("incrementTime")
+    })
+
+    it("dispatches decrementTime when the decrement button is clicked", () => {
+        const button = wrapper.find(".button__decrement-time")
+        store.dispatch = jest.fn()
+
+        button.trigger("click")
+
+        expect(store.dispatch).toHaveBeenCalledWith("decrementTime")
+    })
+})
