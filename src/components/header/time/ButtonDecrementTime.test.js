4a41ddfafc955819c98bd26a78a5f49f2e1770b0
Lasse Malmberg <lasse@lassemalmberg.com>
2020-05-23 03:46:54 +0300

    Add tests for time buttons

---
+++ src/components/header/time/ButtonDecrementTime.test.js
@@ -0,0 +1,35 @@
+import { mount, createLocalVue } from "@vue/test-utils"
+import ButtonDateTime from "@/components/header/ButtonDateTime.vue"
+import ButtonDecrementTime from "./ButtonDecrementTime.vue"
+
+const localVue = createLocalVue()
+const mockStore = { dispatch: jest.fn() }
+
+function customMount (computed = {}) {
+    return mount(ButtonDecrementTime, {
+        localVue,
+        computed,
+        mocks: {
+            $store: mockStore
+        }
+    })
+}
+
+describe("ButtonDecrementTime.vue", () => {
+    let wrapper
+
+    beforeEach(() => {
+        wrapper = customMount({ isFirstTimeSelected: () => false })
+    })
+
+    it("is disabled when the first available time is selected", () => {
+        expect(wrapper.findComponent(ButtonDateTime).props().disabled).toBe(false)
+        wrapper = customMount({ isFirstTimeSelected: () => true })
+        expect(wrapper.findComponent(ButtonDateTime).props().disabled).toBe(true)
+    })
+
+    it("dispatches an action when it is clicked", async () => {
+        await wrapper.find("button").trigger("click")
+        expect(mockStore.dispatch).toHaveBeenCalledWith("decrementTime")
+    })
+})
