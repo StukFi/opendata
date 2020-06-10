42e5b6dfdb211fd3f6a243f43819b55b82fd9d81
Lasse Malmberg <lasse@lassemalmberg.com>
2020-06-10 16:29:36 +0300

    Add app version number to settings page

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,21 @@
+const webpack = require("webpack")
+const fs = require("fs")
+
+const packageJSON = fs.readFileSync("./package.json")
+const version = JSON.parse(packageJSON).version || 0
+
 module.exports = {
     publicPath: "./",
     productionSourceMap: false,
     configureWebpack: {
-        devtool: "source-map"
+        devtool: "source-map",
+        plugins: [
+            new webpack.DefinePlugin({
+                "process.env": {
+                    APP_VERSION: "'" + version + "'"
+                }
+            })
+        ]
     },
     pluginOptions: {
         i18n: {
