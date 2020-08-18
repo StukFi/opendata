a454ff78eb3983a41f9046a653d6bd3f68e6f2a3
Lasse Malmberg <lasse@lassemalmberg.com>
2020-08-18 18:04:02 +0300

    Remove obsolete mutation

--- src/store/modules/settings/mutations.js
+++ src/store/modules/settings/mutations.js
@@ -2,8 +2,5 @@ export default {
     setSettings (state, settings) {
         state.settings = settings
         state.settings.save()
-    },
-    toggleMapLegendBar (state, index) {
-        state.settings.mapLegend.toggleThreshold(index)
     }
 }
