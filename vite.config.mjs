c967eff2001c9188dd49e37eefa8dc4d3a539702
Atte Lähepelto <alahepelto@gmail.com>
2024-06-28 14:12:12 +0300

    Production build performance improvement: Chunk splitting

--- vite.config.mjs
+++ vite.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
       vue: 'vue/dist/vue.esm-bundler.js',
       '@': path.resolve(pathSegments, './src'),
     },
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+    extensions: ['.mjs', '.js', '.json', '.vue'],
   },
   css: {
     preprocessorOptions: {
@@ -33,6 +33,19 @@ export default defineConfig({
     'import.meta.env.APP_VERSION': JSON.stringify(version),
     'import.meta.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
     global: {}
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // Splits plotly.js to its own chunk
+        manualChunks(id) {
+          if (id.includes('node_modules') && id.includes('plotly.js')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1100, // Suppresses warning for large plotly.js chunk
   },
   test: {
     environment: 'jsdom',
