8fabd72e7d47de9eaad3d2230783ac02ea0f1970
Atte Lähepelto <alahepelto@gmail.com>
2024-07-05 12:56:19 +0300

    Changed type to "module"

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from 'axios'
 
 export default {
     async get(url) {
         try {
-            const response = await axios.get(url);
-            return response;
+            const response = await axios.get(url)
+            return response
         } catch (error) {
-            throw error;
+            throw error
         }
     }
 }
